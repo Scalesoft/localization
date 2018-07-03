86f9d424798c55c22eab00b15310f8933e772f4c
Lukas Toman <lukas.toman@scalesoft.cz>
2018-07-03 14:15:05 +0200

    Refactoring replaced string constants by enums.

--- Solution/Localization.AspNetCore.Service/AttributeStringLocalizer.cs
+++ Solution/Localization.AspNetCore.Service/AttributeStringLocalizer.cs
@@ -8,37 +8,27 @@ using Microsoft.Extensions.Localization;
 namespace Localization.AspNetCore.Service
 {
     /// <summary>
-    /// IStringLocalizer implementation is required for supporting localization of the ViewModel attributes e.g. [Name("stringKey")].
+    ///     IStringLocalizer implementation is required for supporting localization of the ViewModel attributes e.g.
+    ///     [Name("stringKey")].
     /// </summary>
     public class AttributeStringLocalizer : IStringLocalizer
     {
         private const string CultureCookieName = "Localization.Culture";
 
-        public const string SourceDictionaryFileKey = "File";
-        public const string SourceDictionaryDatabaseKey = "Database";
-        public const string SourceDictionaryAutoKey = "Auto";
+        private readonly IAutoLocalizationManager m_autoLocalizationManager;
+        private readonly string m_baseName;
+        private readonly IAutoDictionaryManager m_dictionaryManager;
 
         private readonly IHttpContextAccessor m_httpContextAccessor;
+        private readonly LocTranslationSource m_location;
 
         private CultureInfo m_currentCultureInfo;
 
-        private readonly IAutoLocalizationManager m_autoLocalizationManager;
-        private readonly IAutoDictionaryManager m_dictionaryManager;
-        private readonly string m_baseName;
-        private readonly string m_location;
-
-        private readonly Dictionary<string, LocTranslationSource> m_sourcesDictonary = 
-            new Dictionary<string, LocTranslationSource>();
-
-        private AttributeStringLocalizer(IAutoDictionaryManager dictionaryManager, 
-                                        IHttpContextAccessor httpContextAccessor, 
-                                        IAutoLocalizationManager autoLocalizationManager,
-                                        string baseName, string location, CultureInfo cultureInfo)
+        private AttributeStringLocalizer(IAutoDictionaryManager dictionaryManager,
+            IHttpContextAccessor httpContextAccessor,
+            IAutoLocalizationManager autoLocalizationManager,
+            string baseName, LocTranslationSource location, CultureInfo cultureInfo)
         {
-            m_sourcesDictonary.Add(SourceDictionaryFileKey, LocTranslationSource.File);
-            m_sourcesDictonary.Add(SourceDictionaryDatabaseKey, LocTranslationSource.Database);
-            m_sourcesDictonary.Add(SourceDictionaryAutoKey, LocTranslationSource.Auto);
-
             m_dictionaryManager = dictionaryManager;
             m_baseName = baseName;
             m_location = location;
@@ -50,57 +40,43 @@ namespace Localization.AspNetCore.Service
         public AttributeStringLocalizer(IAutoDictionaryManager dictionaryManager,
             IHttpContextAccessor httpContextAccessor,
             IAutoLocalizationManager autoLocalizationManager,
-            string baseName, string location) : this(dictionaryManager, httpContextAccessor, autoLocalizationManager, baseName, location, null)
+            string baseName, LocTranslationSource location) : this(dictionaryManager, httpContextAccessor,
+            autoLocalizationManager, baseName, location, null)
         {
             //Should be empty
         }
 
-        
         public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
         {
-            return m_dictionaryManager.GetDictionary(ParseLocation(m_location), RequestCulture(), m_baseName).Values;
+            return m_dictionaryManager.GetDictionary(m_location, RequestCulture(), m_baseName).Values;
         }
 
         public IStringLocalizer WithCulture(CultureInfo culture)
         {
-            return new AttributeStringLocalizer(m_dictionaryManager, m_httpContextAccessor, m_autoLocalizationManager, m_baseName, m_location, culture);
+            return new AttributeStringLocalizer(m_dictionaryManager, m_httpContextAccessor, m_autoLocalizationManager,
+                m_baseName, m_location, culture);
         }
 
-        LocalizedString IStringLocalizer.this[string name]
-        {
-            get { return m_autoLocalizationManager.Translate(ParseLocation(m_location), name, RequestCulture(), m_baseName); }
-        }
+        LocalizedString IStringLocalizer.this[string name] =>
+            m_autoLocalizationManager.Translate(m_location, name, RequestCulture(), m_baseName);
 
-        
-        LocalizedString IStringLocalizer.this[string name, params object[] arguments]
-        {
-            get { return m_autoLocalizationManager.TranslateFormat(ParseLocation(m_location), name, arguments, RequestCulture(), m_baseName); }
-        }
 
-        private LocTranslationSource ParseLocation(string locationName)
-        {
-            return m_sourcesDictonary[locationName];
-        }
+        LocalizedString IStringLocalizer.this[string name, params object[] arguments] =>
+            m_autoLocalizationManager.TranslateFormat(m_location, name, arguments, RequestCulture(), m_baseName);
 
         /// <summary>
-        /// Culture filter method.
+        ///     Culture filter method.
         /// </summary>
         /// <param name="cultureInfo">Culture info</param>
         /// <returns>If cultureInfo param is not null, paramater value is returned. Else CultureInfo from cookie.</returns>
         private CultureInfo RequestCulture(CultureInfo cultureInfo = null)
         {
-            if (cultureInfo != null)
-            {
-                return cultureInfo;
-            }
+            if (cultureInfo != null) return cultureInfo;
 
             HttpRequest request = m_httpContextAccessor.HttpContext.Request;
 
             string cultureCookie = request.Cookies[CultureCookieName];
-            if (cultureCookie == null)
-            {
-                cultureCookie = m_dictionaryManager.DefaultCulture().Name;
-            }
+            if (cultureCookie == null) cultureCookie = m_dictionaryManager.DefaultCulture().Name;
 
             return new CultureInfo(cultureCookie);
         }
--- Solution/Localization.AspNetCore.Service/Factory/AttributeStringLocalizerFactory.cs
+++ Solution/Localization.AspNetCore.Service/Factory/AttributeStringLocalizerFactory.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using Localization.CoreLibrary.Common;
 using Localization.CoreLibrary.Manager;
+using Localization.CoreLibrary.Util;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Localization;
-using Microsoft.Extensions.Logging;
 
 namespace Localization.AspNetCore.Service.Factory
 {
     /// <summary>
-    /// AttributeStringLocalizerFactory for creating IStringLocalizer instances
+    ///     AttributeStringLocalizerFactory for creating IStringLocalizer instances
     /// </summary>
     public class AttributeStringLocalizerFactory : IStringLocalizerFactory
     {
+        private readonly IAutoLocalizationManager m_autoLocalizationManager;
         private readonly IAutoDictionaryManager m_dictionaryManager;
         private readonly IHttpContextAccessor m_httpContextAccessor;
-        private readonly IAutoLocalizationManager m_autoLocalizationManager;
 
         public AttributeStringLocalizerFactory(IHttpContextAccessor httpContextAccessor)
         {
-            m_dictionaryManager = Localization.CoreLibrary.Localization.Dictionary;           
-            m_autoLocalizationManager = Localization.CoreLibrary.Localization.Translator;
+            m_dictionaryManager = CoreLibrary.Localization.Dictionary;
+            m_autoLocalizationManager = CoreLibrary.Localization.Translator;
             m_httpContextAccessor = httpContextAccessor;
         }
 
@@ -27,7 +27,8 @@ namespace Localization.AspNetCore.Service.Factory
         {
             Guard.ArgumentNotNull(nameof(resourceSource), resourceSource);
 
-            return new AttributeStringLocalizer(m_dictionaryManager, m_httpContextAccessor, m_autoLocalizationManager, resourceSource.Name, AttributeStringLocalizer.SourceDictionaryAutoKey);
+            return new AttributeStringLocalizer(m_dictionaryManager, m_httpContextAccessor, m_autoLocalizationManager,
+                resourceSource.Name, LocTranslationSource.Auto);
         }
 
         public IStringLocalizer Create(string baseName, string location)
@@ -35,8 +36,11 @@ namespace Localization.AspNetCore.Service.Factory
             Guard.ArgumentNotNullOrEmpty(nameof(baseName), baseName);
             Guard.ArgumentNotNullOrEmpty(nameof(location), location);
 
-            return new AttributeStringLocalizer(m_dictionaryManager, m_httpContextAccessor, m_autoLocalizationManager, baseName, location);
+            if (Enum.TryParse<LocTranslationSource>(location, out var parsedLocation))
+                return new AttributeStringLocalizer(m_dictionaryManager, m_httpContextAccessor,
+                    m_autoLocalizationManager, baseName, parsedLocation);
+
+            throw new ArgumentException($"Location string \"{location}\" is not valid enum.");
         }
     }
-
 }
