898d70ede2187cecafbac486aee21a2b91a45fb3
Vladimír Pokorný <vladimir.pokorny@scalesoft.cz>
2018-10-08 17:27:25 +0200

    Unit test for concurrent access to library

---
+++ Solution/Localization.CoreLibrary.Tests/Translator/ConcurrentTranslateTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Localization.CoreLibrary.Dictionary.Factory;
+using Localization.CoreLibrary.Manager.Impl;
+using Localization.CoreLibrary.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Localization.CoreLibrary.Tests.Translator
+{
+    [TestClass]
+    public class ConcurrentTranslateTest
+    {
+        [TestMethod]
+        public void TranslateConcurrentlyTest()
+        {
+            var configuration = new LocalizationConfiguration.Configuration
+            {
+                BasePath = @"localization",
+                DefaultCulture = @"cs",
+                SupportedCultures = new List<string> {"en", "cs"},
+                TranslationFallbackMode = LocTranslateFallbackMode.Key.ToString()
+            };
+
+            var localizationConfiguration = new LocalizationConfiguration(configuration);
+
+            var dictionaryManager = new FileDictionaryManager(localizationConfiguration);
+            var loadedDictionaries = dictionaryManager.AutoLoadDictionaries(JsonDictionaryFactory.FactoryInstance);
+            dictionaryManager.BuildDictionaryHierarchyTrees(loadedDictionaries);
+
+            var fileLocalizationManager = new FileLocalizationManager(localizationConfiguration);
+            fileLocalizationManager.AddDictionaryManager(dictionaryManager);
+
+
+            var availableKeys = new[]
+            {
+                "cancel",
+                "retry",
+                "delete",
+                "close",
+                "yes",
+                "no",
+                "ok",
+            };
+
+            Parallel.For(0, 10000, iteration =>
+            {
+                var key = availableKeys[iteration % availableKeys.Length];
+                fileLocalizationManager.Translate(key);
+            });
+
+            var ls = fileLocalizationManager.TranslateFormat("klíč-stringu", new[] { "pondělí" }, new CultureInfo(configuration.DefaultCulture), "global");
+
+            Assert.AreEqual("Dnes je pondělí.", ls.Value);
+        }
+    }
+}
