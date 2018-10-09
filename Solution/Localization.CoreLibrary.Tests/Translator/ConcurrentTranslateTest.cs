2f2486aad59220e83bf1e67005f56d396da49dd0
Vladimír Pokorný <vladimir.pokorny@scalesoft.cz>
2018-10-09 09:38:53 +0200

    Add test for concurrent dictionary enumerating

--- Solution/Localization.CoreLibrary.Tests/Translator/ConcurrentTranslateTest.cs
+++ Solution/Localization.CoreLibrary.Tests/Translator/ConcurrentTranslateTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Localization.CoreLibrary.Dictionary.Factory;
 using Localization.CoreLibrary.Manager.Impl;
@@ -10,27 +11,33 @@ namespace Localization.CoreLibrary.Tests.Translator
     [TestClass]
     public class ConcurrentTranslateTest
     {
-        [TestMethod]
-        public void TranslateConcurrentlyTest()
+        private FileLocalizationManager m_fileLocalizationManager;
+        private FileDictionaryManager m_dictionaryManager;
+
+        [TestInitialize]
+        public void Init()
         {
             var configuration = new LocalizationConfiguration.Configuration
             {
                 BasePath = @"localization",
                 DefaultCulture = @"cs",
-                SupportedCultures = new List<string> {"en", "cs"},
+                SupportedCultures = new List<string> { "en", "cs" },
                 TranslationFallbackMode = LocTranslateFallbackMode.Key.ToString()
             };
 
             var localizationConfiguration = new LocalizationConfiguration(configuration);
 
-            var dictionaryManager = new FileDictionaryManager(localizationConfiguration);
-            var loadedDictionaries = dictionaryManager.AutoLoadDictionaries(JsonDictionaryFactory.FactoryInstance);
-            dictionaryManager.BuildDictionaryHierarchyTrees(loadedDictionaries);
+            m_dictionaryManager = new FileDictionaryManager(localizationConfiguration);
+            var loadedDictionaries = m_dictionaryManager.AutoLoadDictionaries(JsonDictionaryFactory.FactoryInstance);
+            m_dictionaryManager.BuildDictionaryHierarchyTrees(loadedDictionaries);
 
-            var fileLocalizationManager = new FileLocalizationManager(localizationConfiguration);
-            fileLocalizationManager.AddDictionaryManager(dictionaryManager);
+            m_fileLocalizationManager = new FileLocalizationManager(localizationConfiguration);
+            m_fileLocalizationManager.AddDictionaryManager(m_dictionaryManager);
+        }
 
-
+        [TestMethod]
+        public void TranslateConcurrentlyTest()
+        {
             var availableKeys = new[]
             {
                 "cancel",
@@ -45,7 +52,20 @@ namespace Localization.CoreLibrary.Tests.Translator
             Parallel.For(0, 10000, iteration =>
             {
                 var key = availableKeys[iteration % availableKeys.Length];
-                fileLocalizationManager.Translate(key);
+                m_fileLocalizationManager.Translate(key);
+            });
+        }
+
+        [TestMethod]
+        public void EnumerateDictionariesTest()
+        {
+            Parallel.For(0, 2000, iteration =>
+            {
+                var dictionary = m_dictionaryManager.GetDictionary();
+                foreach (var keyValuePair in dictionary)
+                {
+                    Assert.IsNotNull(keyValuePair);
+                }
             });
         }
     }
