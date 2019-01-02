db4143c361e205860f6336ee67fc25e277f8ad5b
Jan Trejbal <jan.trejbal@scalesoft.cz>
2019-01-02 14:55:45 +0100

    Fix cross platform tests

--- Solution/Localization.CoreLibrary.Tests/Util/Impl/FolderScannerTest.cs
+++ Solution/Localization.CoreLibrary.Tests/Util/Impl/FolderScannerTest.cs
@@ -54,7 +54,7 @@ namespace Localization.CoreLibrary.Tests.Util.Impl
                 configuration, Path.Combine("Localization", "slovniky"), new CultureInfo("cs")
             );
 
-            Assert.AreEqual("Localization/slovniky/slovniky.cs.json", fileName);
+            Assert.AreEqual(Path.GetFullPath("Localization/slovniky/slovniky.cs.json"), Path.GetFullPath(fileName));
         }
     }
 }
