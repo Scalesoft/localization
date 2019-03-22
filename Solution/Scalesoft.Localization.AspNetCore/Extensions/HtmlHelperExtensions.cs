ee8dabb4335d4a3de9614a7c1b2c82999444ae18
Vladimír Pokorný <vladimir.pokorny@scalesoft.cz>
2019-03-22 10:15:00 +0100

    Add extensions methods for printing encoded text as IHtmlContent

---
+++ Solution/Scalesoft.Localization.AspNetCore/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Scalesoft.Localization.AspNetCore.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlContent EncodedString(this IHtmlHelper htmlHelper, string value)
+        {
+            return new StringHtmlContent(value);
+        }
+
+        public static IHtmlContent EncodedString(this IHtmlHelper htmlHelper, object value, IFormatProvider formatProvider = null)
+        {
+            var formattedString = Convert.ToString(value, formatProvider);
+            return new StringHtmlContent(formattedString);
+        }
+    }
+}
