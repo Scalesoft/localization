fe34b488e1b7f7e5f34db681eee74b9101e24361
Lukas Toman <lukas.toman@scalesoft.cz>
2019-04-03 17:21:57 +0200

    Added optional culture parameter when retrieving dynamic text

--- Solution/Scalesoft.Localization.AspNetCore/IDynamicTextService.cs
+++ Solution/Scalesoft.Localization.AspNetCore/IDynamicTextService.cs
@@ -7,7 +7,7 @@ namespace Scalesoft.Localization.AspNetCore
 {
     public interface IDynamicTextService
     {
-        DynamicText GetDynamicText(string name, string scope);
+        DynamicText GetDynamicText(string name, string scope, CultureInfo culture = null);
         DynamicText SaveDynamicText(DynamicText dynamicText, IfDefaultNotExistAction actionForDefaultCulture = IfDefaultNotExistAction.DoNothing);
         IList<DynamicText> GetAllDynamicText(string name, string scope);
         void DeleteAllDynamicText(string name, string scope);
