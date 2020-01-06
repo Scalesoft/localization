14af8867adaf33240493442f8865263202191fa7
Vladimír Pokorný <vladimir.pokorny@scalesoft.cz>
2020-01-06 18:19:16 +0100

    Gulp for TypeScript compile

---
+++ Solution/Scalesoft.Localization.Tool.Translator/gulpfile.js
@@ -0,0 +1,35 @@
+﻿/// <binding BeforeBuild='yarn-runtime' ProjectOpened='default, yarn-runtime' />
+
+var gulp = require("gulp"),
+    sourcemaps = require("gulp-sourcemaps"),
+    ts = require("gulp-typescript"),
+    yarn = require("gulp-yarn");
+var tsProject = ts.createProject("tsconfig.json");
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+
+
+// TypeScript build
+gulp.task("build:ts", function () {
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return tsResult.js
+        .pipe(sourcemaps.write(".", { sourceRoot: "." }))
+        .pipe(gulp.dest(paths.webroot + "js"));
+});
+
+
+//Download yarn dependencies
+
+gulp.task("yarn-runtime", function () {
+    return gulp.src(["./wwwroot/package.json", "./wwwroot/yarn.lock"])
+        .pipe(yarn());
+});
+
+// Main build
+
+gulp.task("default", gulp.parallel("build:ts"));
