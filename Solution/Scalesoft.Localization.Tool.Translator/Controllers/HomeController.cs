edd8c832e8f3d4771923006404ab3801fd07ae73
Vladimír Pokorný <vladimir.pokorny@scalesoft.cz>
2020-01-06 15:32:06 +0100

    Create empty project for Translator tool

---
+++ Solution/Scalesoft.Localization.Tool.Translator/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Scalesoft.Localization.Tool.Translator.Models;
+
+namespace Scalesoft.Localization.Tool.Translator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> m_logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            m_logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Solution/Scalesoft.Localization.Tool.Translator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Scalesoft.Localization.Tool.Translator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
