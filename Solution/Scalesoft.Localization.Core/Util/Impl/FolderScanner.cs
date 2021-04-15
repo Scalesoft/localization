d74900727e2ea5814b012418a6f7673e06a88f81
Denis Senchenko <denis.senchenko@scalesoft.cz>
2021-04-15 12:02:44 +0200

    Tweak log messages. Do not throw on missing global dictionary file during dictionaries autoload

--- Solution/Scalesoft.Localization.Core/Util/Impl/FolderScanner.cs
+++ Solution/Scalesoft.Localization.Core/Util/Impl/FolderScanner.cs
@@ -72,25 +72,27 @@ namespace Scalesoft.Localization.Core.Util.Impl
                 if (!supportedCultureResourceFileFound)
                 {
                     var message = string.Format(
-                        @"Cannot init library. Dictionary file ""{0}.[{1}]"" is missing.",
+                        @"Global dictionary file ""{0}.[{1}]"" is missing. This may lead to library initialization failure if it is not provided otherwise.",
                         supportedFilePathWithoutExtension,
                         string.Join("|", m_dictionaryFactory.FileExtensions)
                     );
-                    if (m_logger != null && m_logger.IsErrorEnabled())
+                    if (m_logger != null && m_logger.IsWarningEnabled())
                     {
-                        m_logger.LogError(message);
+                        m_logger.LogWarning(message);
                     }
 
-                    // global scope must be present in a culture even if a culture is dynamic user-created
-                    // it is required to build hierarchy
-                    exceptionLogStringBuilder.AppendLine(message);
-                    shouldThrowException = true;
+                    /* global scope must be present in a culture even if a culture is dynamic user-created
+                    it is required to build hierarchy
+                    global dictionary however may be loaded manually*/
+                    //exceptionLogStringBuilder.AppendLine(message);
+                    //shouldThrowException = true;
                 }
             }
 
             if (shouldThrowException)
             {
-                throw new DictionaryLoadException(string.Concat("Missing resource file/s.", '\n', exceptionLogStringBuilder.ToString()));
+                throw new DictionaryLoadException(string.Concat("Missing resource file/s.", '\n',
+                    exceptionLogStringBuilder.ToString()));
             }
 
             return localizationFiles;
@@ -102,7 +104,8 @@ namespace Scalesoft.Localization.Core.Util.Impl
         /// </summary>
         /// <param name="libConfiguration">Library configuration.</param>
         /// <param name="scopeDirectories">Array with all folders inside localization folder</param>
-        private IEnumerable<string> CheckScopeResourceFiles(LocalizationConfiguration libConfiguration, string[] scopeDirectories)
+        private IEnumerable<string> CheckScopeResourceFiles(LocalizationConfiguration libConfiguration,
+            string[] scopeDirectories)
         {
             IList<string> localizationFiles = new List<string>();
 
@@ -123,7 +126,8 @@ namespace Scalesoft.Localization.Core.Util.Impl
 
                     foreach (var fileExtension in m_dictionaryFactory.FileExtensions)
                     {
-                        var currentPath = ConstructResourceFileName(libConfiguration, scopeDirectory, supportedCulture, fileExtension);
+                        var currentPath = ConstructResourceFileName(libConfiguration, scopeDirectory, supportedCulture,
+                            fileExtension);
 
                         if (File.Exists(currentPath))
                         {
@@ -137,10 +141,10 @@ namespace Scalesoft.Localization.Core.Util.Impl
                     if (!currentResourceFileFound)
                     {
                         var message = string.Format(
-                            @"Cannot init library. Dictionary file ""{0}[{1}]"" is missing.",
+                            @"Dictionary file ""{0}[{1}]"" is missing.",
                             ConstructResourceFileName(libConfiguration, scopeDirectory, supportedCulture, ""),
                             string.Join("|", m_dictionaryFactory.FileExtensions)
-                            );
+                        );
                         if (m_logger != null && m_logger.IsInformationEnabled())
                         {
                             m_logger.LogInformation(message);
@@ -191,7 +195,8 @@ namespace Scalesoft.Localization.Core.Util.Impl
 
             if (shouldThrowException)
             {
-                throw new DictionaryLoadException(string.Concat("Missing resource file/s.", '\n', exceptionLogStringBuilder.ToString()));
+                throw new DictionaryLoadException(string.Concat("Missing resource file/s.", '\n',
+                    exceptionLogStringBuilder.ToString()));
             }
 
             return localizationFiles;
@@ -214,13 +219,15 @@ namespace Scalesoft.Localization.Core.Util.Impl
         /// <param name="directory"></param>
         /// <param name="cultureInfo"></param>
         /// <returns></returns>
-        public string ConstructResourceFileName(LocalizationConfiguration libConfiguration, string directory, CultureInfo cultureInfo, string fileExtension)
+        public string ConstructResourceFileName(LocalizationConfiguration libConfiguration, string directory,
+            CultureInfo cultureInfo, string fileExtension)
         {
             var cutString = string.Concat(libConfiguration.BasePath, Path.DirectorySeparatorChar);
             var nameBase = directory.Split(new[] {cutString}, StringSplitOptions.None);
             if (nameBase.Length != 2)
             {
-                var message = string.Format(@"Provided path ""{0}"" is not inside basepath: ""{1}""", directory, libConfiguration.BasePath);
+                var message = string.Format(@"Provided path ""{0}"" is not inside basepath: ""{1}""", directory,
+                    libConfiguration.BasePath);
                 if (m_logger != null && m_logger.IsErrorEnabled())
                 {
                     m_logger.LogError(message);
